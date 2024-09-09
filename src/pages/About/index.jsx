86a06d25da5fdaddf45e93451f9753680f62c887
William Cardoso <williamcardoso@gmail.com>
2024-09-09 19:13:26 -0300

    estilização da home e uso da Api na mesma

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -6,7 +6,7 @@ export default function About(){
         <div>
             <h1>Página Sobre</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia corporis consequuntur perferendis, placeat veniam, eum amet minima, id molestiae cumque eligendi pariatur nulla illum error ratione fugiat consequatur deserunt exercitationem.
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia corporis consequuntur perferendis, placeat veniam, eum amet minima, id molestiae cumque eligendi pariatur nulla illum error ratione fugiat consequatur deserunt exercitationem.
             </p>
         </div>
     )
