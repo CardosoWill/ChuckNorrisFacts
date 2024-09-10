6ae8ed81d907f06bb76785bf1df65a8296adf52e
William Cardoso <williamcardoso@gmail.com>
2024-09-09 21:11:16 -0300

    Estilização de login e criação de registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Register from './pages/Register'
 import Login from './pages/login'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -10,12 +11,14 @@ import Footer from './components/Footer'
 function App() {
   return (
     <>
+
       <Header />
       <Routes>
-        <Route path='/' element={<Login />} />
         <Route path='/home' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/fatos' element={<Fatos/>} />
+        <Route path='/fatos' element={<Fatos />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
       <Footer />
     </>
