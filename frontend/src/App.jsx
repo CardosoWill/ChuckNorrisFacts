6ee5e23b84fe80392f8d5323c67c494a9d361d5e
JoseReche <jose.carlosreche@gmail.com>
2024-10-28 21:39:54 -0300

    final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,32 +4,40 @@ import Welcome from './pages/Welcome'
 import Register from './pages/Register'
 import Login from './pages/login'
 import Piadas from './pages/Piadas'
-import About from './pages/About'
+import User from './pages/User'
 import Fatos from './pages/Fatos'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { AuthProvider } from './auth/Context'
+import PrivateRoute from './routes/PrivateRoute'
+import VerifyCode from './pages/VerifyCode'
 
 function App() {
 
   const location = useLocation();
 
-  const showElement = location.pathname === '/login' ||  location.pathname === '/register';
+  const showElement = location.pathname === '/login' ||  location.pathname === '/register' || location.pathname ==='/'|| location.pathname ==='/verify-code';
 
   return (
-    <>
+    <AuthProvider>
     
     {!showElement?<Header/>:null}
 
       <Routes>
-      <Route path='/' element={<Welcome />} />
+      <Route path='/' element={<Welcome/>} />
+        
+        <Route element={<PrivateRoute />}>
         <Route path='/piadas' element={<Piadas />} />
-        <Route path='/about' element={<About />} />
+        <Route path='/user' element={<User />} />
         <Route path='/fatos' element={<Fatos />} />
+        </Route>
+        
         <Route path='/login' element={<Login />} />
+        <Route path='/verify-code' element={<VerifyCode />} />
         <Route path='/register' element={<Register />} />
       </Routes>
       {!showElement?<Footer/>:null}
-    </>
+    </AuthProvider>
   )
 }
 
--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,12 +1,49 @@
+import { useContext, useState } from 'react';
 import './styles.css';
 import { useNavigate } from 'react-router-dom';
+import { authContext } from '../../auth/Context';
+import { loginUser } from '../../api/user';
 
 export default function Login() {
+  const  { login }  = useContext(authContext)
   const navigate = useNavigate();
 
   const apertaBotao = (event) => {
     event.preventDefault();
-    navigate('/piadas');
+    navigate('/');
+  };
+  /*
+    const handleBackClick = () => {
+      navigate('/')
+    };
+  
+    const handleCreateAccont = () => {
+      navigate('/login')
+    };
+  */
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      //return toast("Informe o E-mail e senha para continuar!")
+    }
+
+    try {
+      const response = await loginUser(email, password)
+      if (response.token) {
+        login(response.token);
+        navigate('/piadas')
+      }
+
+    } catch (error) {
+      /*if (error.response.status === 403) {
+        return toast("Email ou senha invalido");
+      }
+      return toast("Erro inesperado, tente novamente mais tarde")*/
+    }
   };
 
   return (
@@ -15,10 +52,10 @@ export default function Login() {
         <form className="login-form" onSubmit={apertaBotao}>
           <p className="login-title">Login de Usuário</p>
 
-          <input className="login-input" type="email" placeholder="Email" required />
-          <input className="login-input" type="password" placeholder="Senha" required />
+          <input className="login-input" type="email" required value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+          <input className="login-input" type="password" required value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Senha" />
 
-          <button className="login-btn" type="submit">Login</button>
+          <button className="login-btn" type="submit" onClick={handleSubmit}>Login</button>
 
           <p className="login-signin">Não tem um login? <a href="/register" className="login-link">Clique aqui</a></p>
         </form>
